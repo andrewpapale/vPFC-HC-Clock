dfec49d6689394137d2dc45058fc49be573dc17b
andrewpapale <papalae04@gmail.com>
2025-04-24 11:00:18 -0400

    Create R2R_Plot_vmax_and_ventropy_MMClock_Explore.R

---
+++ R2R_Plot_vmax_and_ventropy_MMClock_Explore.R
@@ -0,0 +1,232 @@
+# 2025-04-24 AndyP
+# Plot v_entropy and v_max timecourses for R2R
+
+repo_directory <- "~/clock_analysis"
+
+source('/Users/dnplserv/clock_analysis/fmri/keuka_brain_behavior_analyses/dan/get_trial_data.R')
+df <- get_trial_data(repo_directory='/Volumes/Users/Andrew/MEDuSA_data_Explore',dataset='explore')
+df <- df %>%
+  group_by(id,run_number) %>%
+  arrange(id, run_number, trial) %>%
+  mutate(condition_trial = run_trial-floor(run_trial/40.5)*40,
+         condition_trial_neg_inv = -1000 / condition_trial,
+         condition_trial_neg_inv_sc = as.vector(scale(condition_trial_neg_inv)),
+  ) %>% ungroup()
+df <- df %>%
+  group_by(id) %>% 
+  mutate(v_entropy_sc_r = scale(v_entropy)) %>% ungroup() %>%
+  group_by(id, run) %>% 
+  mutate(v_chosen_sc = scale(v_chosen),
+         abs_pe_max_sc = scale(abs(pe_max)),
+         score_sc = scale(score_csv),
+         iti_sc = scale(iti_ideal),
+         iti_prev_sc = scale(iti_prev),
+         v_chosen_sc = scale(v_chosen),
+         pe_max_sc = scale(pe_max),
+         pe_max_lag_sc = scale(lag(pe_max)),
+         abs_pe_max_lag_sc = scale(abs(pe_max_lag)),
+         rt_vmax_sc = scale(rt_vmax),
+         ev_sc = scale(ev),
+         v_entropy_sc = scale(v_entropy),
+         v_max_lag_sc = scale(lag(v_max)),
+         v_entropy_wi_change_lag = lag(v_entropy_wi_change),
+         abs_rt_vmax_change = abs(rt_vmax_change),
+         rt_vmax_change_bin = case_when(
+           abs_rt_vmax_change < 4/24 ~ 'No Change',
+           abs_rt_vmax_change >= 4/24 ~ 'Change'
+         ),
+         v_entropy_wi_change_bin = case_when(
+           v_entropy_wi_change < -0.5 ~ 'Decrease',
+           v_entropy_wi_change > 0.5  ~ 'Increase',
+           v_entropy_wi_change >= -0.5 & v_entropy_wi_change <= 0.5 ~ 'No Change'
+         ),
+         rt_vmax_change_sc = scale(rt_vmax_change)) %>% arrange(id, run, run_trial) %>% mutate(log10kld4 = case_when(
+           kld4 ==0 ~ NA_real_,
+           kld4 >0 ~ log10(kld4)
+         )) %>% mutate(log10kld4_lag = case_when(
+           kld4_lag==0 ~NA_real_,
+           kld4_lag>0 ~ log10(kld4_lag)
+         ))
+df <- df %>% group_by(id,run) %>% mutate(expl_longer =(case_when(
+  rt_csv - rt_lag > 1 ~ 'Longer',
+  rt_csv - rt_lag < -1 ~ '0',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run) %>% mutate(expl_shorter =(case_when(
+  rt_csv - rt_lag > 1 ~ '0',
+  rt_csv - rt_lag < -1 ~ 'Shorter',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run)  %>% mutate(rt_bin = (case_when(
+  rt_csv_sc <= -1 ~ '-1',
+  rt_csv_sc > -1 & rt_csv_sc <= 0 ~ '-0.5',
+  rt_csv_sc > 0 & rt_csv_sc <= 1 ~ '0.5',
+  rt_csv_sc > 1 ~ '1'
+)))
+df <- df %>% group_by(id,run) %>% mutate(trial_bin = (case_when(
+  run_trial <= 13 ~ 'Early',
+  run_trial > 13 & run_trial < 26 ~ 'Middle',
+  run_trial >=26 ~ 'Late',
+)))
+
+df1 <- df
+rm(df)
+
+source('/Users/dnplserv/clock_analysis/fmri/keuka_brain_behavior_analyses/dan/get_trial_data.R')
+df <- get_trial_data(repo_directory=repo_directory,dataset='mmclock_fmri')
+df <- df %>% 
+  group_by(id, run) %>% 
+  mutate(v_chosen_sc = scale(v_chosen),
+         abs_pe_max_sc = scale(abs(pe_max)),
+         iti_lag_sc = scale(iti_prev),
+         score_sc = scale(score_csv),
+         score_lag_sc = scale(lag(score_csv)),
+         iti_sc = scale(iti_ideal),
+         pe_max_sc = scale(pe_max),
+         pe_max_lag_sc = scale(lag(pe_max)),
+         abs_pe_max_lag_sc = scale(abs(pe_max_lag)),
+         rt_vmax_sc = scale(rt_vmax),
+         ev_sc = scale(ev),
+         ev_lag_sc = scale(lag(ev)),
+         v_entropy_sc = scale(v_entropy),
+         v_entropy_lag_sc = scale(lag(v_entropy)),
+         v_max_lag_sc = scale(lag(v_max)),
+         v_entropy_wi_change_lag = lag(v_entropy_wi_change),
+         abs_rt_vmax_change = abs(rt_vmax_change),
+         rt_vmax_change_bin = case_when(
+           abs_rt_vmax_change < 4/24 ~ 'No Change',
+           abs_rt_vmax_change >= 4/24 ~ 'Change'
+         ),
+         v_entropy_wi_change_lag_bin = case_when(
+           v_entropy_wi_change_lag < -0.5 ~ 'Decrease',
+           v_entropy_wi_change_lag > 0.5  ~ 'Increase',
+           v_entropy_wi_change_lag >= -0.5 & v_entropy_wi_change_lag <= 0.5 ~ 'No Change'
+         ),
+         rt_vmax_change_sc = scale(rt_vmax_change)) %>% arrange(id, run, run_trial) %>% mutate(log10kld4 = case_when(
+           kld4 ==0 ~ NA_real_,
+           kld4 >0 ~ log10(kld4)
+         )) %>% mutate(log10kld4_lag = case_when(
+           kld4_lag==0 ~NA_real_,
+           kld4_lag>0 ~ log10(kld4_lag)
+         ))
+df <- df %>% group_by(id,run) %>% mutate(expl_longer =(case_when(
+  rt_csv - rt_lag > 1 ~ 'Longer',
+  rt_csv - rt_lag < -1 ~ '0',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run) %>% mutate(expl_shorter =(case_when(
+  rt_csv - rt_lag > 1 ~ '0',
+  rt_csv - rt_lag < -1 ~ 'Shorter',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run)  %>% mutate(rt_bin = (case_when(
+  rt_csv_sc <= -1 ~ '-1',
+  rt_csv_sc > -1 & rt_csv_sc <= 0 ~ '-0.5',
+  rt_csv_sc > 0 & rt_csv_sc <= 1 ~ '0.5',
+  rt_csv_sc > 1 ~ '1'
+)))
+df <- df %>% group_by(id,run) %>% mutate(trial_bin = (case_when(
+  run_trial <= 15 ~ 'Early',
+  run_trial > 15 & run_trial < 30 ~ 'Middle',
+  run_trial >=30 ~ 'Late',
+)))
+
+df2 <- df
+rm(df)
+
+source('/Users/dnplserv/clock_analysis/fmri/keuka_brain_behavior_analyses/dan/get_trial_data.R')
+df <- get_trial_data(repo_directory=repo_directory,dataset='mmclock_meg')
+df <- df %>% 
+  group_by(id, run) %>% 
+  mutate(v_chosen_sc = scale(v_chosen),
+         abs_pe_max_sc = scale(abs(pe_max)),
+         iti_lag_sc = scale(iti_prev),
+         score_sc = scale(score_csv),
+         score_lag_sc = scale(lag(score_csv)),
+         pe_max_sc = scale(pe_max),
+         pe_max_lag_sc = scale(lag(pe_max)),
+         abs_pe_max_lag_sc = scale(abs(pe_max_lag)),
+         rt_vmax_sc = scale(rt_vmax),
+         ev_sc = scale(ev),
+         ev_lag_sc = scale(lag(ev)),
+         v_entropy_sc = scale(v_entropy),
+         v_entropy_lag_sc = scale(lag(v_entropy)),
+         v_max_lag_sc = scale(lag(v_max)),
+         v_entropy_wi_change_lag = lag(v_entropy_wi_change),
+         abs_rt_vmax_change = abs(rt_vmax_change),
+         rt_vmax_change_bin = case_when(
+           abs_rt_vmax_change < 4/24 ~ 'No Change',
+           abs_rt_vmax_change >= 4/24 ~ 'Change'
+         ),
+         v_entropy_wi_change_lag_bin = case_when(
+           v_entropy_wi_change_lag < -0.5 ~ 'Decrease',
+           v_entropy_wi_change_lag > 0.5  ~ 'Increase',
+           v_entropy_wi_change_lag >= -0.5 & v_entropy_wi_change_lag <= 0.5 ~ 'No Change'
+         ),
+         rt_vmax_change_sc = scale(rt_vmax_change)) %>% arrange(id, run, run_trial) %>% mutate(log10kld4 = case_when(
+           kld4 ==0 ~ NA_real_,
+           kld4 >0 ~ log10(kld4)
+         )) %>% mutate(log10kld4_lag = case_when(
+           kld4_lag==0 ~NA_real_,
+           kld4_lag>0 ~ log10(kld4_lag)
+         ))
+df <- df %>% group_by(id,run) %>% mutate(expl_longer =(case_when(
+  rt_csv - rt_lag > 1 ~ 'Longer',
+  rt_csv - rt_lag < -1 ~ '0',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run) %>% mutate(expl_shorter =(case_when(
+  rt_csv - rt_lag > 1 ~ '0',
+  rt_csv - rt_lag < -1 ~ 'Shorter',
+  rt_csv - rt_lag < 1 & rt_csv - rt_lag > -1 ~ '0'
+)))
+df <- df %>% group_by(id,run)  %>% mutate(rt_bin = (case_when(
+  rt_csv_sc <= -1 ~ '-1',
+  rt_csv_sc > -1 & rt_csv_sc <= 0 ~ '-0.5',
+  rt_csv_sc > 0 & rt_csv_sc <= 1 ~ '0.5',
+  rt_csv_sc > 1 ~ '1'
+)))
+df <- df %>% group_by(id,run) %>% mutate(trial_bin = (case_when(
+  run_trial <= 15 ~ 'Early',
+  run_trial > 15 & run_trial < 30 ~ 'Middle',
+  run_trial >=30 ~ 'Late',
+)))
+
+df3 <- df
+rm(df)
+
+df1 <- df1 %>% mutate(dataset = 'Experiment 2')
+df2 <- df2 %>% mutate(dataset = 'Experiment 1 - fMRI')
+df3 <- df3 %>% mutate(dataset = 'Experiment 1 - MEG')
+
+
+demo <- readRDS('/Volumes/Users/Andrew/MEDuSA_data_Explore/explore_n146.rds')
+demo <- demo %>% select(registration_redcapid,age,gender,registration_group,wtar,education_yrs)
+demo <- demo %>% rename(id=registration_redcapid,group=registration_group)
+demo$gender <- relevel(as.factor(demo$gender),ref='M')
+demo$age <- scale(demo$age)
+demo$wtar <- scale(demo$wtar)
+demo$education_yrs <- scale(demo$education_yrs)
+
+demo$id <- as.character(demo$id)
+
+df1 <- inner_join(df1, demo, by=c('id'))
+
+demo <- read.table(file=file.path(repo_directory, 'fmri/data/mmy3_demographics.tsv'),sep='\t',header=TRUE)
+demo <- demo %>% rename(id=lunaid)
+demo <- demo %>% select(!adult & !scandate)
+demo$female <- relevel(as.factor(demo$female),ref='0')
+demo$age <- scale(demo$age)
+
+demo$id <- as.character(demo$id)
+df2$id <- as.character(df2$id)
+
+df2 <- inner_join(df2, demo, by=c('id'))
+df3 <- inner_join(df3, demo, by=c('id'))
+
+
+df1 <- df1 %>% filter(group=='HC')  %>% select(id,run,trial,run_trial,v_entropy,v_entropy_wi,v_max,v_max_wi) 
+df2 <- df2 %>% select(id,run,trial,run_trial,v_entropy,v_entropy_wi,v_max,v_max_wi) 
+df3 <- df3 %>% select(id,run,trial,run_trial,v_entropy,v_entropy_wi,v_max,v_max_wi) 
+
+df <- rbind(df1,df2,df3)
