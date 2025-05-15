c906b8b4e1338fe1d3247bef0bdb2d2f15d24f41
andrewpapale <papalae04@gmail.com>
2025-05-15 17:39:25 -0400

    Update parametric_modulator.m

--- parametric_modulator.m
+++ parametric_modulator.m
@@ -5,7 +5,7 @@
 % first import test1.csv (entropy for 202200, run 1)
 TR = 0.6;
 nTR = length(rdmU1(:,3)); % 3 is dmUBLOCK(1), for now
-onsets = interp1(1:nTR,1:nTR,test(:,4)./TR);
+onsets = interp1(1:nTR,1:nTR,test(:,4)./TR,'nearest');
 parameter = test1;
 PM = zeros(nTR,1); 
 PM(onsets) = parameter / max(parameter);
