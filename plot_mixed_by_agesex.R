b9a6a7279e2e6fd0bb44d9058a48bda27855f8d4
andrewpapale <papalae04@gmail.com>
2025-06-10 09:55:23 -0400

    Update plot_mixed_by_agesex.R

--- plot_mixed_by_agesex.R
+++ plot_mixed_by_agesex.R
@@ -23,5 +23,20 @@ ddqRT <- ddqRT %>% mutate(dataset1 = case_when(dataset == 'fMRI' ~ 'Experiment 1
                                                dataset == 'MEG' ~ 'Experiment 1 - MEG',
                                                dataset == 'bsocial' ~ 'Experiment 2')) %>% select(!dataset) %>% rename(dataset = dataset1)
 
+
+fills <- palette()
+fills[1] <- '#E618E6'
+fills[2] <- '#181FE6'
+
+
 ggplot(data=ddqRT, aes(x=sex,y=rt_lag_sc.trend,color=sex,group=sex,ymin=rt_lag_sc.trend-std.error,ymax=rt_lag_sc.trend+std.error)) + 
-  geom_errorbar() + facet_grid(~dataset,scales = 'free_y') + scale_y_reverse()
+  geom_errorbar(width=0.65,size=1.2) + geom_point(size=2) + facet_grid(~dataset,scales = 'free_y') + scale_y_reverse() + 
+  scale_colour_manual(values=fills) +  
+  ylab('<-- less -- Exploration -- more -->') +
+  theme(legend.title = element_blank(),
+        axis.title.y = element_text(margin=margin(r=6),size=22),
+        axis.title.x = element_text(margin=margin(t=6),size=22),
+        legend.text = element_text(size=22),
+        axis.text.x = element_text(size=22),
+        axis.text.y = element_text(size=22),
+        strip.text = element_text(size=22))
